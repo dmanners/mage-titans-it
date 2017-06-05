ac931b6cbe658281fbdd5aa0864ad052cfc9194e
David Manners <dmanners87@gmail.com>
2017-06-05 19:12:43 +0000

    Test both functions in the stock object

--- tests/Domain/Stock/StockTest.php
+++ tests/Domain/Stock/StockTest.php
@@ -6,12 +6,47 @@ use PHPUnit\Framework\TestCase;
 
 final class StockTest extends TestCase
 {
-    public function testGetInStock()
+    /**
+     * @dataProvider getInStockProvider
+     */
+    public function testGetInStock($inStock, $expectedInStock)
     {
         $stock = new Stock(
-            false,
-            0
+            $inStock,
+            100
         );
-        $this->assertFalse($stock->getInStock());
+        $this->assertEquals($expectedInStock, $stock->getInStock());
+    }
+
+    public function getInStockProvider()
+    {
+        return [
+            [true, true],
+            [false, false],
+            [1, 1],
+            [0, 0]
+        ];
+    }
+
+    /**
+     * @dataProvider getStockLevelProvider
+     */
+    public function testGetStockLevel($stockLevel, $expectedStockLevel)
+    {
+        $stock = new Stock(
+            true,
+            $stockLevel
+        );
+        $this->assertEquals($expectedStockLevel, $stock->getStockLevel());
+    }
+
+    public function getStockLevelProvider()
+    {
+        return [
+            [300, 300],
+            [20, 20],
+            [0, 0],
+            [5, 5]
+        ];
     }
 }
